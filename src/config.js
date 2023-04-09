3789946e56b812162390fef9b32e7b1b1d91bb70
leaps <lmtejedor@gmail.com>
2023-04-09 12:11:21 -0400

    add files

---
+++ src/config.js
@@ -0,0 +1,330 @@
+var config = {
+    style: 'mapbox://styles/leamarie/clg72cq0l00bd01o2urixfju9',
+    accessToken: 'pk.eyJ1IjoibGVhbWFyaWUiLCJhIjoiUzllXzJqayJ9.ZPC8TyOilUnFD3-_9yOu3g',
+    showMarkers: false,
+    markerColor: '#3FB1CE',
+    //projection: 'equirectangular',
+    //Read more about available projections here
+    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
+    inset: true,
+    theme: 'dark',
+    use3dTerrain: true, //set true for enabling 3D maps.
+    auto: false,
+    title: 'Continuing to Prepare for the End of Title 42',
+    subtitle: '',
+    byline: '',
+    footer: '',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: false,
+            title: '',
+            image: 'images/1.jpg',
+            description: '',
+            location: {
+                center: [-106.4245, 31.6904],
+                zoom: 8.5,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+        {
+            id: 'two',
+            alignment: 'left',
+            hidden: false,
+            title: '',
+            image: 'images/2.jpg',
+            description: '',
+            location: {
+                center: [-106.395340, 31.682859],
+                zoom: 18,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+        {
+            id: 'two-one',
+            alignment: 'left',
+            hidden: false,
+            title: '',
+            image: 'images/2.1.jpg',
+            description: '',
+            location: {
+                center: [-106.395340, 31.682859],
+                zoom: 18,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+        {
+            id: 'two-two',
+            alignment: 'left',
+            hidden: false,
+            title: '',
+            image: 'images/2.2.jpg',
+            description: '',
+            location: {
+                center: [-106.395340, 31.682859],
+                zoom: 18,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+        {
+            id: 'two-three',
+            alignment: 'left',
+            hidden: false,
+            title: '',
+            image: 'images/2.3.jpg',
+            description: '',
+            location: {
+                center: [-106.395340, 31.682859],
+                zoom: 18,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+        {
+            id: 'two-four',
+            alignment: 'left',
+            hidden: false,
+            title: '',
+            image: 'images/2.4.jpg',
+            description: '',
+            location: {
+                center: [-106.421116, 31.690925],
+                zoom: 14,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+
+        {
+            id: 'second-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: '',
+            image: 'images/3.jpg',
+            description: '',
+            location: {
+                center: [-94.1300, 17.5000],
+                zoom: 4,
+                pitch: 60,
+                bearing: -43.2,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+
+        {
+            id: 'second-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: '',
+            image: 'images/3.1.jpg',
+            description: '',
+            location: {
+                center: [-94.1300, 17.5000],
+                zoom: 4,
+                pitch: 60,
+                bearing: -43.2,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+
+        {
+            id: 'second-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: '',
+            image: 'images/3.2.jpg',
+            description: '',
+            location: {
+                center: [-94.1300, 17.5000],
+                zoom: 4,
+                pitch: 60,
+                bearing: -43.2,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+
+
+        {
+            id: 'second-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: '',
+            image: 'images/3.3.jpg',
+            description: '',
+            location: {
+                center: [-94.1300, 17.5000],
+                zoom: 4,
+                pitch: 60,
+                bearing: -43.2,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+
+        {
+            id: 'third-identifier',
+            alignment: 'left',
+            hidden: false,
+            title: '',
+            image: 'images/9.jpg',
+            description: '',
+            location: {
+                center: [-106.421116, 31.690925],
+                zoom: 12.52,
+                pitch: 8.01,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+    ]
+};
